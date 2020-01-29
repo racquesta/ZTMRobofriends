1eb8c222f662f2ced628fc22da611908c3e6bc61
racquesta <racquesta@trilogyed.com>
2020-01-29 09:22:40 -0500

    Updates

---
+++ precache-manifest.b3c763265264cc6b58c00f2b08c85051.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7d9c3bf3d45da3b7681951219e5f30",
+    "url": "/ZTMrobofriends/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/ZTMrobofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8bdcb8ad5baddeb39071",
+    "url": "/ZTMrobofriends/static/css/main.92b6a449.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/ZTMrobofriends/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ZTMrobofriends/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bdcb8ad5baddeb39071",
+    "url": "/ZTMrobofriends/static/js/main.a17e36fa.chunk.js"
+  },
+  {
+    "revision": "52d14b808832c48b6588",
+    "url": "/ZTMrobofriends/static/js/runtime-main.bdb3ba56.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ZTMrobofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
